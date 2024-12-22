090a85841320ac313bb9c67ffd5779fc4f252511
Farid Muhammad Eryanto <fbrstudio06@gmail.com>
2024-12-22 14:27:06 +0700

    Website Routing

--- addons/faridacademy/controllers/route.py
+++ addons/faridacademy/controllers/route.py
@@ -3,6 +3,14 @@ from odoo import http
 
 class RouteAcademy(http.Controller):
 
+    @http.route('/academy/<name>/', auth='public', website=True)
+    def academy(self, name):
+        return '<h1>{}</h1>'.format(name)
+
+    @http.route('/academy/type/<int:id>/', auth='public', website=True)
+    def data_type(self, id):
+        return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+
     @http.route('/sample', auth='public')
     def sample(self, **kw):
         print("Hello, world")
@@ -19,4 +27,10 @@ class RouteAcademy(http.Controller):
         Courses = http.request.env['faridacademy.course'].sudo().search([])
         return http.request.render('faridacademy.course', {
             'courses': Courses,
+        })
+
+    @http.route('/course/<model("faridacademy.course"):course>/', auth='public', website=True)
+    def course_detail(self, course):
+        return http.request.render('faridacademy.course_detail', {
+            'course': course
         })
